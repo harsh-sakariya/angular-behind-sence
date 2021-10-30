9e4871b2742c409e7a76ffe4983a5fb7f40690c0
harsh-sakariya <harshs@prominentpixel.com>
2021-10-30 17:38:07 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'behind-scenes';
+export class AppComponent implements DoCheck {
+  students = [
+    {
+      firstName: "harsh",
+      lastName: "sakariya",
+      rollNo: 101
+    },
+    {
+      firstName: "test1",
+      lastName: "testing1",
+      rollNo: 102
+    },
+    {
+      firstName: "test2",
+      lastName: "testing2",
+      rollNo: 103
+    },
+    {
+      firstName: "test3",
+      lastName: "testing3",
+      rollNo: 104
+    }
+  ];
+
+  ngDoCheck(){
+    console.log("ngDoCheck is called");
+  }
+
+  onAddStudent(){
+    const newStudents = [
+      {
+        firstName: "test4",
+        lastName: "testing4",
+        rollNo: 105
+      },
+      {
+        firstName: "test5",
+        lastName: "testing5",
+        rollNo: 106
+      }
+    ]
+    this.students.push(...newStudents);  
+  }
+
+  onChangeFirstName(){
+    for(const student of this.students){
+      student.firstName = "firstName";
+    }
+  }
+
+  onChangeInstance(){
+    this.students = this.students.slice();
+  }
 }
